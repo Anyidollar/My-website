0e59229df8da6ebd1f470eff279ad24d58ff597e
Anyidollar <ifeanyionyekwere247@gmail.com>
2023-12-09 23:24:39 +0100

    seventh commit

--- My-web/ifeanyi-web/src/components/Footer/Footer.js
+++ My-web/ifeanyi-web/src/components/Footer/Footer.js
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa";
 import { Button } from "../../globalStyles";
 import {
   FooterContainer,
@@ -12,6 +19,12 @@ import {
   FooterLinksItems,
   FooterLinkTitle,
   FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcon,
+  SocialIconLink,
 } from "./Footer.elements";
 
 const Footer = () => {
@@ -37,6 +50,7 @@ const Footer = () => {
             <FooterLink to="/">Investors</FooterLink>
             <FooterLink to="/">Terms of Service</FooterLink>
           </FooterLinksItems>
+
           <FooterLinksItems>
             <FooterLinkTitle>Contact us</FooterLinkTitle>
             <FooterLink to="/sign-up">How it works</FooterLink>
@@ -45,6 +59,8 @@ const Footer = () => {
             <FooterLink to="/">Investors</FooterLink>
             <FooterLink to="/">Terms of Service</FooterLink>
           </FooterLinksItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
           <FooterLinksItems>
             <FooterLinkTitle>Videos</FooterLinkTitle>
             <FooterLink to="/sign-up">How it works</FooterLink>
@@ -71,7 +87,28 @@ const Footer = () => {
           </SocialLogo>
           <WebsiteRights>ULTRA © 2023</WebsiteRights>
           <socialIcons>
-            
+            <SocialIconLink href="/" target="_blank" arial-label="Facebook">
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" arial-label="Instagram">
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink
+              href={
+                "//www.youtube.com/channel/UCsksymTY_4BYR-wytLjex7A? view_as=subcriber"
+              }
+              target="_blank"
+              arial-label="Youtube"
+              rel="noopener noreferrer"
+            >
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" arial-label="Twitter">
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" arial-label="Linkedin">
+              <FaLinkedin />
+            </SocialIconLink>
           </socialIcons>
         </SocialMediaWrap>
       </SocialMedia>
