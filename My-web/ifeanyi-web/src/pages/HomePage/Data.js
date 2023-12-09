b5e6274902dde1caebe4222339775d02587981ee
Anyidollar <ifeanyionyekwere247@gmail.com>
2023-12-09 18:18:13 +0100

    fifth commit

--- My-web/ifeanyi-web/src/pages/HomePage/Data.js
+++ My-web/ifeanyi-web/src/pages/HomePage/Data.js
@@ -1,17 +1,71 @@
+import svgImage from "../../images/svg-1.svg";
+import svgImage2 from "../../images/svg-2.svg"
+import svgImage3 from "../../images/svg-3.svg"
+import svgImage4 from "../../images/svg-4.svg"
 export const homeObjOne = {
   lightBg: false,
   primary: true,
-  lightBg: false,
   buttonLabel: "Get Started",
-  imgStart: "",
+  imgstart: "",
   lightTopLine: true,
   topLine: "Marketing Agency",
   lightText: true,
   headline: "Lead Generation Specialist for Online Businesses",
   lightTextDesc: true,
   description:
-    "We help Business Owners Increases Their Revenue. Our Team of Specialist Can Help You Achieve Your Goals.",
-    img: require(),
-    alt: "Image",
-    start: ""
+    "We help Business Owners Increase Their Revenue. Our Team of Specialists Can Help You Achieve Your Goals.",
+  img: svgImage,
+  alt: "Image",
+  start: "",
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  primary: true,
+  buttonLabel: "Get Started",
+  imgstart: "start",
+  lightTopLine: false,
+  topLine: "Marketing Agency",
+  lightText: false,
+  headline: "Lead Generation Specialist for Online Businesses",
+  lightTextDesc: false,
+  description:
+    "We help Business Owners Increase Their Revenue. Our Team of Specialists Can Help You Achieve Your Goals.",
+  img: svgImage2,
+  alt: "Image",
+  start: "true",
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  primary: true,
+  buttonLabel: "Get Started",
+  imgstart: "",
+  lightTopLine: true,
+  topLine: "Marketing Agency",
+  lightText: true,
+  headline: "Lead Generation Specialist for Online Businesses",
+  lightTextDesc: true,
+  description:
+    "We help Business Owners Increase Their Revenue. Our Team of Specialists Can Help You Achieve Your Goals.",
+  img: svgImage3,
+  alt: "Image",
+  start: "",
+};
+
+export const homeObjFour = {
+  lightBg: false,
+  primary: true,
+  buttonLabel: "Get Started",
+  imgstart: "start",
+  lightTopLine: true,
+  topLine: "Marketing Agency",
+  lightText: true,
+  headline: "Lead Generation Specialist for Online Businesses",
+  lightTextDesc: true,
+  description:
+    "We help Business Owners Increase Their Revenue. Our Team of Specialists Can Help You Achieve Your Goals.",
+  img: svgImage4,
+  alt: "Image",
+  start: "true",
 };
